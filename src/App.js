84994c8b1e4b6f95591d9f4a2f19e120dc5c756d
maxly20 <maxly2011@gmail.com>
2021-06-19 18:28:51 +1000

    adding photos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Components
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+// Pages
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Gallery from './pages/Gallery/Gallery';
+import Portfolio from './pages/Portfolio/Portfolio';
+
 const App = () => {
   return (
-    <div className='app'>
-      <div className='app__wrapper'>HELOOOO</div>
-    </div>
+    <Router>
+      <div className='app'>
+        {/* <div className='app__wrapper'> */}
+          <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/about' exact component={About} />
+            <Route path='/contact' exact component={Contact} />
+            <Route path='/gallery' exact component={Gallery} />
+            <Route path='/portfolio' exact component={Portfolio} />
+          </Switch>
+          <Footer />
+        {/* </div> */}
+      </div>
+    </Router>
   );
 };
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,32 @@
 import './Navbar.scss';
+import { Link } from 'react-router-dom';
+import Logo from '../../images/logo.png';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav class='navbar'>
+      <Link to='/'>
+        <img class='logo' src={Logo} alt='' />
+      </Link>
+      <ul class='nav-links'>
+        <li>
+          <Link to='/'>home</Link>
+        </li>
+        <li>
+          <Link to='/about'>about</Link>
+        </li>
+        <li>
+          <Link to='/portfolio'>portfolio</Link>
+        </li>
+        <li>
+          <Link to='/gallery'>gallery</Link>
+        </li>
+        <li>
+          <Link to='/contact'>contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
