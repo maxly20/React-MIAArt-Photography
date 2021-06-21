a770f356d30c81365b3280cd8ee90db54b8d26c6
maxly20 <maxly2011@gmail.com>
2021-06-21 21:43:13 +1000

    building slider 2

---
+++ src/components/Slider/Slider2.js
@@ -0,0 +1,21 @@
+import './Slider.scss';
+
+// import slide_one from '../../images/home/01.jpg';
+// import slide_two from '../../images/home/02.jpg';
+// import slide_three from '../../images/home/03.jpg';
+// import slide_four from '../../images/home/04.jpg';
+// import slide_five from '../../images/home/05.jpg';
+// import slide_six from '../../images/home/06.jpg';
+// import slide_seven from '../../images/home/07.jpg';
+// import slide_eight from '../../images/home/08.jpg';
+
+
+const Slider2 = () => {
+    return (
+        <div>
+            Slider2
+        </div>
+    )
+}
+
+export default Slider2
