9bff317e513ca9853240e5c279bbe849928f898e
maxly20 <maxly2011@gmail.com>
2021-06-27 21:56:01 +1000

    building header

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,29 @@
 import './Home.scss';
-import Slider from '../../components/Slider/Slider';
+import Slider2 from '../../components/Slider/Slider2';
+// Image
+import slide_one from '../../images/home/01.jpg';
+import slide_two from '../../images/home/02.jpg';
+import slide_three from '../../images/home/03.jpg';
+import slide_four from '../../images/home/04.jpg';
+import slide_five from '../../images/home/05.jpg';
+import slide_six from '../../images/home/06.jpg';
+import slide_seven from '../../images/home/07.jpg';
+import slide_eight from '../../images/home/08.jpg';
 
 const Home = () => {
+  const homeImages = [
+    slide_one,
+    slide_two,
+    slide_three,
+    slide_four,
+    slide_five,
+    slide_six,
+    slide_seven,
+    slide_eight,
+  ];
   return (
     <div className='featured_container'>
-      <Slider />
+      <Slider2 images={homeImages} />
     </div>
   );
 };
